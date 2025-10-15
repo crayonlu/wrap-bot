1048c7807295ec5f4a808a0a12cb568b042c6461
crayon <crayonlu@qq.com>
2025-10-15 12:27:48 +0800

    fix: import path

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -3,7 +3,6 @@ package plugins
 import (
 	"github.com/crayon/bot_golang/internal/config"
 	"github.com/crayon/bot_golang/pkgs/bot"
-	tech_push "github.com/crayon/bot_golang/plugins/tech_push"
 )
 
 func Register(engine *bot.Engine, cfg *config.Config) {
@@ -13,6 +12,6 @@ func Register(engine *bot.Engine, cfg *config.Config) {
 		engine.Use(AIChatPlugin(cfg))
 	}
 	if cfg.HotApiHost != "" && cfg.HotApiKey != "" {
-		engine.Use(tech_push.TechPushPlugin(cfg))
+		engine.Use(TechPushPlugin(cfg))
 	}
 }
