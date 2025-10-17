c646e696910d37b74ffb4728080dd6e61411d31b
crayon <crayonlu@qq.com>
2025-10-17 11:30:43 +0800

    test: trigger sche

--- internal/tasks/tech_push_task.go
+++ internal/tasks/tech_push_task.go
@@ -52,7 +52,7 @@ func (t *TechPushTask) Schedule(sched *scheduler.Scheduler, cfg *config.Config) 
 		return nil
 	}
 
-	_, err := sched.At(10, 0, 0).WithID(t.Name()).Do(func() {
+	_, err := sched.At(12, 0, 0).WithID(t.Name()).Do(func() {
 		if err := t.service.SendTechPush(t.cache); err != nil {
 			log.Printf("TechPushTask execution failed: %v", err)
 		} else {
