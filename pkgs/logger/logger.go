b59e9308abd351e4be75353c9c74b359814ed5c1
crayon <crayonlu@qq.com>
2025-11-03 21:12:41 +0800

    feat: enhance logging by replacing log.Println with log.Printf for structured log messages

--- pkgs/logger/logger.go
+++ pkgs/logger/logger.go
@@ -64,6 +64,8 @@ func (l *Logger) Log(level LogLevel, message string, context map[string]interfac
 		l.entries = l.entries[1:]
 	}
 
+	log.Printf("[%s] %s", string(level), message)
+
 	if l.broadcastFn != nil {
 		go l.broadcastFn(entry)
 	}
@@ -114,27 +116,22 @@ func (l *Logger) SetBroadcastFunc(fn func(LogEntry)) {
 }
 
 func Info(message string) {
-	log.Println(message)
 	globalLogger.Info(message)
 }
 
 func Warn(message string) {
-	log.Println(message)
 	globalLogger.Warn(message)
 }
 
 func Error(message string) {
-	log.Println(message)
 	globalLogger.Error(message)
 }
 
 func Debug(message string) {
-	log.Println(message)
 	globalLogger.Debug(message)
 }
 
 func InfoWithContext(message string, context map[string]interface{}) {
-	log.Println(message)
 	globalLogger.InfoWithContext(message, context)
 }
 
